47be2b0f7046c1fc0ed10ae0f11afb61f68f1053
ValenMR <valentinamunoz1010@gmail.com>
2023-11-08 08:23:30 -0500

    configurando firebase e importando react

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAVWIJubtG55orYXsjg2A_45mAhJBvDnBY",
+  authDomain: "crud-structuras-react.firebaseapp.com",
+  projectId: "crud-structuras-react",
+  storageBucket: "crud-structuras-react.appspot.com",
+  messagingSenderId: "199350327313",
+  appId: "1:199350327313:web:fd8979108afa23787e79b0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
