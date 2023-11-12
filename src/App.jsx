e7475b78abdaa6109652a92bc053561975b421fa
ValenMR <valentinamunoz1010@gmail.com>
2023-11-12 16:15:33 -0500

    Se configuró el componente de Show y el enrutador en App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import './App.css';
 import React from 'react';
 
+//importar nuestros componentes
+import { Show } from './components/Show';
+import Edit from './components/Edit';
+import Create from './components/Create';
+
+//importar lo necesario para el enrutador
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
 
-      <h1>hello you</h1>
+      <BrowserRouter>
       
+      </BrowserRouter>
+      
+      <Show />
     </div>
     
   );
--- src/components/Show.js
+++ src/components/Show.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+
 //vamos a impotar lo necesario para trabajar con la base de datos de firestore
-import {collection, getDocs, getDoc, deleteDoc } from 'firebase/firestore'
+import {collection, getDocs, getDoc, deleteDoc, doc } from 'firebase/firestore'
 
 //importar la conexión a la base de datos
 import { db } from '../firabaseConfig/firebase'
@@ -14,20 +15,71 @@ const MySwal = withReactContent(Swal)
 export const Show = () => {
   
   //1 - configurar los hooks
+  const [products, setProducts] = useState( [] )
   
   //2 - referenciamos a la DB firestore
+  const productsCollection = collection(db, "products")
 
   //3 - Función para mostrar TODOS los docs
-
+  const getProducts = async () => {
+    const data = await getDocs(productsCollection)
+    //onsole.log(data.docs)
+    setProducts(
+        data.docs.map( (doc) => ( {...doc.data(), id:doc.id}))
+    )
+    console.log(products)
+  }
   //4 - Función para eliminar un doc
-
+  const deleteProduct = async (id) => {
+    const productDoc = doc(db, "products", id)
+    await deleteDoc(productDoc)
+    getProducts()
+  }
   //5 - Función de confirmación para sweer alert
 
   //6 - usamos useEffect
-
+  useEffect( () => {
+    getProducts()
+    //eslint-disable-next-line
+  }, [] )
   //7 - devolvemos vista de nuestro componente 
-  
+
+  //algo de bootstrap
   return (
-    <div>Show</div>
+    <>
+    <div className='container'>
+        <div className='row'>
+            <div className='col'>
+                <div className='d-grid gap-2'>
+                    <Link to="/create" className='btn btn-secondary mt-2 mb-2'>Crear</Link>
+                </div>
+                
+                <table className='table table-dark table-hover'>
+                    <thead>
+                        <tr>
+                            <th>Description</th>
+                            <th>Stock</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        { products.map( (product) => (
+                            <tr key={product.id}>
+                                <td>{product.description}</td>
+                                <td>{product.stock}</td>
+                                <td>
+                                    <Link to={'/edit/${product.id}'} className='btn btn-light'><i className="fa-solid fa-pencil"></i>Editar</Link>
+                                    <button onClick={ () => { deleteProduct(product.id) } } className='btn btn-danger'><i className="fa-solid fa-trash-can"></i>Borrar</button>
+                                </td>
+                            </tr>
+                        )) }
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    </>
+    
   )
 }
